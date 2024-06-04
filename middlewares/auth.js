b8037254eb016d4685808a55c398d4dccdbe246e
dotarsojat69 <tdeath457@gmail.com>
2024-06-04 14:05:34 +0700

    feat: fixing

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,9 +6,9 @@ const statusMessage = require("../helpers/status.message");
 const secretKey = process.env.SECRET_KEY;
 
 const authentication = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-
+  
   try {
+    const token = req.headers.authorization.split(" ")[1];
     if (token) {
       const decoded = jwt.verify(token, secretKey);
 
